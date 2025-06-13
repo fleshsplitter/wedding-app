1a617c49b5fb6834ce8e17397c60407c33be14c6
Alex Valiulin <alexvaliulin@gmail.com>
2025-06-14 00:23:42 +0800

    change title name

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
+const repoName = 'wedding-app';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`, 
   plugins: [
     react(),
     svgr(),
