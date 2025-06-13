a203c8e8c4296ef4198b79c82c93afc19bf8fcc8
Alex Valiulin <alexvaliulin@gmail.com>
2025-06-13 23:30:16 +0800

    resurrected almost dead project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable import/extensions */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-import './index.css';
+// import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
